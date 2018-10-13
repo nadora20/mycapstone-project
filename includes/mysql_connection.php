51d94ac64e5c1d6807a49c8e53987c0c71096b19
Nader Karam <44067369+nadora20@users.noreply.github.com>
2018-10-13 20:52:16 +0200

    Update mysql_connection.php

--- includes/mysql_connection.php
+++ includes/mysql_connection.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_USER', 'u595095753_root');
-define('DB_PASSWORD', '123698745');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u595095753_cap');
+define('DB_NAME', 'capstonedb');
 
 $dbcl = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
